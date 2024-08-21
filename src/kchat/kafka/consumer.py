27a87ffc43623899c21162ba9786e4bc4bcf8ed5
sooj1n <sujin19371@gmail.com>
2024-08-21 22:16:45 +0900

    결과 확인 완

--- src/kchat/kafka/consumer.py
+++ src/kchat/kafka/consumer.py
@@ -14,32 +14,34 @@ def read_offset():
             return int(f.read().strip())
     return None
 
+saved_offset = read_offset()
 consumer = KafkaConsumer(
-        "topic1",
+        #'topic1',
         bootstrap_servers=['localhost:9092'],
         value_deserializer=lambda x: loads(x.decode('utf-8')),
         consumer_timeout_ms=5000,
-        #저장된게 없으면 처음부터 읽고
-        auto_offset_reset='earliest' if read_offset() is None else 'none',
-        #auto_offset_reset='latest' #'earliest', 'latest'
-        group_id="fbi",
-        enable_auto_commit=True
-)
+        #auto_offset_reset="earliest" if saved_offset is None else 'none',
+        group_id="fbi", #같은그룹으로 묶고 알려준다.
+        enable_auto_commit=False
+        )
 
-print('[Start] get consumer')
-p = TopicPartition('topic1', 0)
+#consumer_timeout_ms=5000 // 타임아웃 5초 후
+#auto_offset_reset=' '    // earliest 이전기록 전부 출력 후 실행, latest 요청 한번만 실행
+print("[Start] get consumer")
+
+p = TopicPartition('topic1', 0) # 0 은 파티션 넘버
 consumer.assign([p])
 
 if saved_offset is not None:
-    consumer.seek(p,saved_offset)
+    consumer.seek(p, saved_offset)
 else:
     consumer.seek_to_beginning(p)
 
-for m in consumer:
-    print(m)
-    #print(f"topic={m.topic}, partition={m.partitions}, offset={m.offset}, value={m.value}, timestamp={msg,timestamp}")
-    print(f"offset={m.offset}, value={m.value}")
-    save_offset(m.offset)
+for msg in consumer:
+    print(f"offset={msg.offset}, value={msg.value}")
+    save_offset(msg.offset + 1)
+	#if msg.value['str'] == 'value9':
+    #    print("Exit message received, closing consumer.")
+    #    break  # 루프 탈출
 
-
-print('[End] get consumer')
+print("[End] get consumer")
